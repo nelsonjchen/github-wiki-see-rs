540b6b843a776dcfda8158257d31dc17bdc282ab
Nelson Chen <crazysim@gmail.com>
2021-03-20 18:31:24 -0700

    hello

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::Logger, post, web, Result};
+
+
+#[get("/mirror/{account}/{repository}")] // <- define path parameters
+async fn mirror(web::Path((account, repository)): web::Path<(String, String)>) -> Result<String> {
+    Ok(format!("Account: {}, Repository {}!", account, repository))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .service(mirror)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+    })
+
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
