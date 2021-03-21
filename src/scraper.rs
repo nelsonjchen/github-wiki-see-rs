7e25e2ecc9675194b7f318cf3cadb9a7e69f2747
Nelson Chen <crazysim@gmail.com>
2021-03-21 14:15:38 -0700

    grabt stuff

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,10 +1,28 @@
+use nipper::Document;
+
+async fn download_github_wiki(
+    account: &str,
+    repository: &str,
+    page: Option<&str>,
+) -> Result<String, reqwest::Error> {
+    let body = reqwest::get(format!(
+        "https://github.com/{}/{}/wiki/{}",
+        account,
+        repository,
+        page.unwrap_or("")
+    ))
+    .await?
+    .text()
+    .await?;
+    Ok(body)
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn something() {
-        use nipper::Document;
-
         let html = r#"<div>
             <a href="/1">One</a>
             <a href="/2">Two</a>
@@ -16,4 +34,29 @@ mod tests {
         let text: &str = &a.text();
         assert_eq!(text, "Three");
     }
+
+    #[test]
+    fn github_html() {
+        let html = include_str!("../test-data/wiki-index.html");
+
+        let document = Document::from(html);
+        let a = document.select("#wiki-wrapper");
+        let text: &str = &a.html();
+        assert_eq!(text, "Three");
+    }
+
+    #[actix_rt::test]
+    async fn download_github_wiki_test() {
+        let html = download_github_wiki(
+            "nelsonjchen",
+             "github-wiki-test",
+              None).await.unwrap();
+
+
+        let document = Document::from(&html);
+        let a = document.select("#wiki-wrapper");
+        let text: &str = &a.html();
+        assert_eq!(text, "Three");
+    }
 }
+
