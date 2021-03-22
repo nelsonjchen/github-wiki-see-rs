db6a2eb8641f5e3dacb8823958fbab395e955761
Nelson Chen <crazysim@gmail.com>
2021-03-21 21:29:42 -0700

    hello zzz

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,20 @@ use actix_web::{
     middleware::Logger,
     post,
     web::{self, scope},
-    App, HttpResponse, HttpServer, Responder, Result,
+    App, HttpRequest, HttpResponse, HttpServer, Responder, Result,
 };
+use askama::Template;
+
 mod scraper;
+
+#[derive(Template)] // this will generate the code...
+#[template(path = "hello.html")] // using the template in this path, relative
+                                 // to the `templates` dir in the crate root
+struct HelloTemplate<'a> {
+    // the name of the struct can be anything
+    name: &'a str, // the field name should match the variable name
+                   // in your template
+}
 
 #[get("/{account}/{repository}")] // <- define path parameters
 async fn mirror_root(
@@ -18,12 +29,17 @@ async fn mirror_root(
 async fn mirror_page(
     web::Path((account, repository, page)): web::Path<(String, String, String)>,
 ) -> Result<String> {
+    let hello = HelloTemplate { name: "world" }; // instantiate your struct
 
-    
     Ok(format!(
         "Account: {}, Repository {}, Page {}!",
         account, repository, page
     ))
+}
+
+async fn front_page(_req: HttpRequest) -> impl Responder {
+    let hello = HelloTemplate { name: "zzz" }; // instantiate your struct
+    hello.render().unwrap()
 }
 
 #[actix_web::main]
@@ -33,6 +49,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
+            .route("/", web::get().to(front_page))
             .service(scope("mirror").service(mirror_root).service(mirror_page))
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
