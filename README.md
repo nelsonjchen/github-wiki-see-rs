56371dfe8924880de815743eeef8e3037c31632d
Nelson Chen <crazysim@gmail.com>
2021-10-06 17:35:37 -0700

    Extensive Decomissioning https://github.com/github/feedback/discussions/4992\#discussioncomment-1439485

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 # GitHub Wiki SEE
 
-*As seen at https://github-wiki-see.page by search engines!*
+*As previously seen at https://github-wiki-see.page by search engines!*
 
-GitHub Wiki Search Engine Enablement is a service to allow GitHub Wikis to be indexed by search engines.
+GitHub Wiki Search Engine Enablement was a service to allow GitHub Wikis to be indexed by search engines.
 
-This is a terribly and hastily built service. However, it is usable and MVP!
+This is a terribly and hastily built service. However, it was usable and MVP!
 
-This was made in response to https://github.com/isaacs/github/issues/1683 which was superceded by https://github.com/github/feedback/discussions/4992.
+This was made in response to https://github.com/github/feedback/discussions/4992 which eventually concluded on October 6, 2021 with the removal of the entry from `robots.txt`.
 
 ## Design
 
-This is designed as a Rust web proxy application. It is not very Rusty and has lots of unwraps, panics, and hackiness. Cleanup, major overhauls, and straightening are much appreciated!
+This was designed as a Rust web proxy application. It was not very Rusty and has lots of unwraps, panics, and hackiness. Cleanup, major overhauls, and straightening are much appreciated!
 
-It is designed to run ondemand as a Docker application on a service such as [Google Cloud Run][gcr]. Uptime and latency are important so that the service appears on search engines and gets a high ranking.
+It was designed to run ondemand as a Docker application on a service such as [Google Cloud Run][gcr]. Uptime and latency were important so that the service appears on search engines and gets a high ranking.
 
-301/302s are intentionally **not** used as to not give search engines the impression that the page is a redirect and to
+301/302s were intentionally **not** used as to not give search engines the impression that the page is a redirect and to
 ignore the content. Humans should see it as a redirect; the robots should not.
 
 All links rendered in the tool going outside of GitHub are tagged with `rel="nofollow ugc"` to prevent ranking
@@ -22,9 +22,9 @@ All links rendered in the tool going outside of GitHub are tagged with `rel="nof
 
 ## Decomissioning
 
-When GitHub removes wikis from their `robots.txt`, this tool will be modified to simply "301" redirect the link to
-GitHub. May this happen someday and soon.
+GitHub had removed wikis from their `robots.txt` and this tool be modified to simply "301" redirect the link to
+GitHub.
 
-If some other unforeseen consequence of the tool happens, this may be done as well.
+https://github.com/github/feedback/discussions/4992#discussioncomment-1439485
 
 [gcr]: https://cloud.google.com/run
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
-use reqwest::{Client, StatusCode};
-use rocket::futures::{FutureExt, TryFutureExt};
 use rocket::http::{ContentType, Status};
-use rocket::response::status;
-use rocket::response::{Redirect, Responder};
-use rocket::State;
+use rocket::response::Redirect;
 
 use askama::Template;
-
-use crate::gh_extensions::github_wiki_markdown_to_pure_markdown;
-use crate::scraper::process_markdown;
-
-mod gh_extensions;
-mod scraper;
 
 #[derive(Template)]
 #[template(path = "front_page.html")]
@@ -36,92 +26,14 @@ fn favicon() -> (Status, (ContentType, &'static [u8])) {
     )
 }
 
-#[get("/robots.txt")]
-fn robots_txt() -> (Status, (ContentType, &'static [u8])) {
-    (
-        Status::Ok,
-        (
-            ContentType::Plain,
-            include_bytes!("../templates/robots.txt"),
-        ),
-    )
-}
-
-#[get("/sitemap.xml")]
-fn sitemap_xml() -> Redirect {
-    Redirect::permanent(uri!(
-        "https://nelsonjchen.github.io/github-wiki-see-rs-sitemaps/sitemap_index.xml"
-    ))
-}
-
-#[get("/base_sitemap.xml")]
-fn base_sitemap_xml() -> Redirect {
-    Redirect::permanent(uri!(
-        "https://nelsonjchen.github.io/github-wiki-see-rs-sitemaps/base_sitemap.xml"
-    ))
-}
-
-#[get("/generated_sitemap.xml")]
-fn generated_sitemap_xml() -> Redirect {
-    Redirect::permanent(uri!(
-        "https://nelsonjchen.github.io/github-wiki-see-rs-sitemaps/generated_sitemap.xml"
-    ))
-}
-
-#[get("/seed_sitemaps/<id>")]
-fn seed_sitemaps(id: &str) -> Redirect {
-    Redirect::permanent(format!(
-        "https://nelsonjchen.github.io/github-wiki-see-rs-sitemaps/seed_sitemaps/{}",
-        id
-    ))
-}
-
-#[derive(Template)]
-#[template(path = "mirror.html")]
-
-struct MirrorTemplate {
-    original_title: String,
-    original_url: String,
-    mirrored_content: String,
-}
-
-#[allow(clippy::large_enum_variant)]
-#[derive(Responder)]
-enum MirrorError {
-    // DocumentNotFound(NotFound<MirrorTemplate>),
-    InternalError(status::Custom<MirrorTemplate>),
-    GiveUpSendToGitHub(Redirect),
-}
-
 #[get("/<account>/<repository>/wiki")]
-async fn mirror_home<'a>(
-    account: &'a str,
-    repository: &'a str,
-    client: &State<Client>,
-) -> Result<MirrorTemplate, MirrorError> {
-    mirror_page(account, repository, "Home", client).await
+async fn mirror_home<'a>(account: &'a str, repository: &'a str) -> Redirect {
+    mirror_page(account, repository, "Home").await
 }
 
 #[get("/<account>/<repository>/wiki/<page>")]
-async fn mirror_page<'a>(
-    account: &'a str,
-    repository: &'a str,
-    page: &'a str,
-    client: &State<Client>,
-) -> Result<MirrorTemplate, MirrorError> {
-    use MirrorError::*;
-
+async fn mirror_page<'a>(account: &'a str, repository: &'a str, page: &'a str) -> Redirect {
     // Check github_assets
-    let raw_github_assets_url = format!(
-        "https://raw.githubusercontent.com/wiki/{}/{}/{}.md",
-        account, repository, page,
-    );
-
-    // Have original URL to forward to if there is an error.
-    let original_url = format!(
-        "https://github.com/{}/{}/wiki/{}",
-        account, repository, page,
-    );
     let original_url_encoded = format!(
         "https://github.com/{}/{}/wiki/{}",
         account,
@@ -129,117 +41,12 @@ async fn mirror_page<'a>(
         percent_encoding::utf8_percent_encode(page, percent_encoding::NON_ALPHANUMERIC),
     );
 
-    let page_title = page.replace("-", " ");
-
-    // Try to grab Stuff
-
-    // Download raw_github_assets_url
-    let resp = client
-        .get(&raw_github_assets_url)
-        .send()
-        .await
-        .map_err(|e| {
-            InternalError(status::Custom(
-                Status::InternalServerError,
-                MirrorTemplate {
-                    original_title: page_title.clone(),
-                    original_url: original_url.clone(),
-                    mirrored_content: format!("500 Internal Server Error - {}", e),
-                },
-            ))
-        })?;
-
-    if resp.status() == StatusCode::NOT_FOUND {
-        // return Err(DocumentNotFound(NotFound(MirrorTemplate {
-        //     original_title: page_title.clone(),
-        //     original_url: original_url.clone(),
-        //     mirrored_content: format!("{}", resp.status()),
-        // })));
-
-        // Just send them onto GitHub
-        return Err(GiveUpSendToGitHub(Redirect::temporary(
-            original_url_encoded,
-        )));
-    }
-
-    if !resp.status().is_success() {
-        return Err(InternalError(status::Custom(
-            Status::InternalServerError,
-            MirrorTemplate {
-                original_title: page_title.clone(),
-                original_url: original_url.clone(),
-                mirrored_content: format!("Remote: {}", resp.status()),
-            },
-        )));
-    }
-
-    let original_markdown = resp.text().await.map_err(|e| {
-        InternalError(status::Custom(
-            Status::InternalServerError,
-            MirrorTemplate {
-                original_title: page_title.clone(),
-                original_url: original_url.clone(),
-                mirrored_content: format!("Internal Server Error - {}", e.to_string()),
-            },
-        ))
-    })?;
-
-    let sidebar_markdown_future = client
-        .get(format!(
-            "https://github.com/{}/{}/wiki/_Sidebar.md",
-            account, repository
-        ))
-        .send()
-        .map(|r| r.map(|r| r.text()))
-        .and_then(|f| f);
-
-    let sidebar_markdown_option: Option<String> = sidebar_markdown_future
-        .await
-        .ok()
-        .map(|s| format!("\n\n# Sidebar\n\n{}", s));
-
-    let content_markdown = {
-        if let Some(sidebar_markdown) = sidebar_markdown_option {
-            format!("{}{}", original_markdown, sidebar_markdown)
-        } else {
-            original_markdown
-        }
-    };
-
-    let pure_markdown =
-        github_wiki_markdown_to_pure_markdown(&content_markdown, account, repository);
-
-    let mirrored_content = if page == "Home" {
-        process_markdown(&pure_markdown, account, repository, true)
-    } else {
-        process_markdown(&pure_markdown, account, repository, false)
-    };
-
-    Ok(MirrorTemplate {
-        original_title: page_title.clone(),
-        original_url: original_url.clone(),
-        mirrored_content,
-    })
+    Redirect::permanent(original_url_encoded)
 }
 
 #[launch]
 fn rocket() -> _ {
-    // Mount front Page
-
-    // Mount Mirror
     rocket::build()
         .mount("/m", routes![mirror_home, mirror_page,])
-        .mount(
-            "/",
-            routes![
-                front,
-                favicon,
-                robots_txt,
-                sitemap_xml,
-                base_sitemap_xml,
-                generated_sitemap_xml,
-                seed_sitemaps
-            ],
-        )
-        .manage(Client::new())
+        .mount("/", routes![front, favicon,])
 }
