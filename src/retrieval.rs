4da42a58a06154cd1c218af9221e8686ddd28088
Nelson Chen <crazysim@gmail.com>
2021-10-09 09:39:16 -0700

    better typing

--- src/retrieval.rs
+++ src/retrieval.rs
@@ -25,7 +25,7 @@ async fn retrieve_source<'a>(
         account, repository, page,
     );
 
-    let resp = client
+    let content = client
         .get(&raw_github_assets_url)
         .send()
         .map_err(|e| ContentError::OtherError(e.to_string()))
@@ -35,11 +35,13 @@ async fn retrieve_source<'a>(
             }
             Ok(r)
         })
-        .map_ok(|r| r.text()
-            .map_err(|e| ContentError::OtherError(e.to_string()))
-        )
+        .map_ok(|r| {
+            r.text()
+                .map_err(|e| ContentError::OtherError(e.to_string()))
+        })
         .and_then(|t| t)
+        .map_ok(|t| Content::Markdown(t))
         .await?;
 
-    Ok(Content::Markdown(resp))
+    Ok(content)
 }
